11596d9c14d7261184db2fd1f2e5a44064a1e05d
Olivier BLANC <olivier.blanc@qiminfo.ch>
2018-03-02 15:37:58 +0100

    Prettify

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -24,10 +24,13 @@ class CommentBox extends Component {
             <form
                 className="comment-box"
                 onSubmit={this.handleSubmit.bind(this)}>
+                    <h4>Add a comment</h4>
                     <textarea
                         value={this.state.comment}
                         onChange={this.handleChange.bind(this)}/>
-                    <button type="submit">Submit comment</button>
+                    <div>
+                        <button type="submit">Submit comment</button>                        
+                    </div>
             </form>
         );
     }
--- test/components/app_test.js
+++ test/components/app_test.js
@@ -2,6 +2,9 @@ import App from  '../../src/components/app';
 import { expect, renderComponent } from '../test_helper';
 
 // describe: Group tests for an app
+// it: Test a single thing
+// expect: Assert the result of a test
+
 describe('App', () => {
   let component;
 
@@ -9,14 +12,12 @@ describe('App', () => {
     component = renderComponent(App);
   });
 
-  // it: Test a single thing
-  it('shows the correct text', () => {
-    // expect: Assert the result of a test
-    expect(component).to.contain('React simple starter');
-  });
-
   it('shows a comment box', () => {
     expect(component.find('.comment-box')).to.exist;
+  });
+
+  it('shows a comment list', () => {
+    expect(component.find('.comment-list')).to.exist;
   });
 
 });
